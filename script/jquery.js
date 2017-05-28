5259ff02ecf29b5fe8363444974a154f005e49c0
Jonathan Sebastian <jojosebseb@gmail.com>
2017-05-28 10:28:28 +0700

    css

--- script/jquery.js
+++ script/jquery.js
@@ -1,28 +1,115 @@
 var src, caption, info, thisInfo;
+var bodySel, topSel, backSel, bindingSel, segmentId;
+var curHigh, curMid, curLow, curId;
+
+var highSel, midSel, lowSel, soundBar;
+
+var totalHigh;
+
+curHigh = 0;
+curMid = 0;
+curLow = 0;
+
+bodySel = $('#bodySel');
+topSel = $('#topSel');
+backSel = $('#backSel');
+bindingSel = $('#bindingSel');
+soundBar = $('.sound-bar');
+
+function toneBar(){
+    $('#hiBar').find('.filler').css({
+        top: (100 - (curHigh*10))+'%'
+    });
+    $('#hiMidBar').find('.filler').css({
+        top: (100-((curHigh+curMid)/2)*10)+'%'
+    });
+    $('#midBar').find('.filler').css({
+        top: (100 - (curMid*10))+'%'
+    });
+    $('#midLowBar').find('.filler').css({
+        top: (100-((curMid+curLow)/2)*10)+'%'
+    });
+    $('#lowBar').find('.filler').css({
+        top: (100 - (curLow*10))+'%'
+    });
+}
+
+function toneSim(){
+    $.each( $('.choice-module'), function(i){
+        if ($(this).hasClass('active')) {
+            highSel+= parseInt($(this).find('.tone-high').html(), 10);
+            midSel+= parseInt($(this).find('.tone-mid').html(), 10);
+            lowSel+= parseInt($(this).find('.tone-low').html(), 10);
+        }
+    })
+
+    curHigh = curHigh + highSel;
+    curMid = curMid + midSel;
+    curLow = curLow + lowSel;
+    console.log('high= '+curHigh);
+}
+
+
+
+$('.choice-module').on('click', function(){
+    $(this).parents('.build-tab').find('.choice-module').removeClass('active');
+    $(this).parents('.build-tab').addClass('active');
+    $(this).addClass('active');
+    caption = $(this).find('.caption').html();
+    $(this).parents('.build-tab').find('.selected-title-summary').html(caption);
+    $(this).parents('.build-tab').find('.title').addClass('active');
+
+    highSel = 0;
+    midSel = 0;
+    lowSel = 0;
+    curHigh = 0;
+    curMid = 0;
+    curLow = 0;
+    toneSim();
+    toneBar();
+
+
+
+    //get segmentId to deteremine which part to be placed in the 'rightFloat'
+    segmentId = $(this).parents('.build-tab').attr('id');
+
+    if (segmentId == 'buildSoundBoard') {
+        topSel.html(caption);
+    }
+    else if (segmentId == 'buildBody') {
+        bodySel.html(caption);
+        highSel = parseInt($(this).find('.tone-high').html());
+        midSel = parseInt($(this).find('.tone-mid').html());
+        lowSel = parseInt($(this).find('.tone-low').html());
+
+        curHigh = 5;
+        curMid = 5;
+        curLow = 5;
+
+        curHigh = curHigh + highSel;
+        curMid = curMid + midSel;
+        curLow = curLow + lowSel;
+
+    }
+    else if (segmentId == 'buildBackSides') {
+        backSel.html(caption);
+    }
+    else if (segmentId == 'bindingBuild') {
+        bindingSel.html(caption);
+    }
+})
+
+
 
 $('.hover-notice').on('click', function(){
-
     thisInfo = $(this).parents('.build-tab').find('.info-container');
     thisInfo.addClass('active');
     src = $(this).parent().find('img').attr('src');
     caption = $(this).parent().find('.caption').html();
     info = $(this).parent().find('.info').html();
-
     thisInfo.find('img').attr('src', src);
     thisInfo.find('.caption-copy').html(caption);
     thisInfo.find('.info-copy').html(info);
-})
-
-$('.choice-module').on('click', function(){
-    $(this).parents('.build-tab').find('.choice-module').removeClass('active');
-
-    $(this).parents('.build-tab').addClass('active');
-    $(this).addClass('active');
-
-    caption = $(this).find('.caption').html();
-    $(this).parents('.build-tab').find('.selected-title-summary').html(caption);
-    $(this).parents('.build-tab').find('.title').addClass('active');
-
 })
 
 $('.close-btn').on('click', function(){
